8fa09e78bbf2d29cb36ad025a4a4c45e107b2b7a
moke <moke@xiaoman.cn>
2019-07-03 09:41:14 +0800

    feat: add rotate array

---
+++ src/sword/6-旋转数组的最小数字.js
@@ -0,0 +1,32 @@
+function minNumberInRotateArray1(rotateArray) {
+  if (rotateArray.length === 0) {
+    return 0
+  }
+
+  for (let i = 0; i < rotateArray.length; i++) {
+    if (rotateArray[i] > rotateArray[i + 1]) {
+      return rotateArray[i + 1]
+    }
+
+    return rotateArray[0]
+  }
+}
+
+function minNumberInRotateArray2(rotateArray) {
+  let left = 0
+  let right = rotateArray.length - 1
+
+  while (right - left > 1) {
+    let mid = (left + (right - left)) >> 1
+
+    mid = left + ((right - left) >> 1)
+
+    if (rotateArray[mid] > rotateArray[right]) {
+      left = mid
+    } else {
+      right = mid
+    }
+  }
+
+  return Math.mid(rotateArray[left], rotateArray[right])
+}
