435245072d2aeb2efc051fa2beed3105c2836005
moke <moke@xiaoman.cn>
2019-08-23 09:54:53 +0800

    feat: add

--- temp.js
+++ temp.js
@@ -1,47 +1,19 @@
-const PENDING = 'pending'
-const FULFILLED = 'fulfilled'
-const REJECTED = 'rejected'
+function inorderTraversal(root) {
+  const stack = []
+  const result = []
 
-class MyPromise {
-  constructor(fn) {
-    this.state = PENDING
-    this.value = null
+  let current = root
 
-    this.fulfilledCallbacks = []
-    this.rejectedCallbacks = []
-
-    const resolve = val => {
-      if (this.state === PENDING) {
-        this.state = FULFILLED
-        this.value = val
-        this.fulfilledCallbacks.forEach(fn => fn())
-      }
-    }
-    const reject = val => {
-      if (this.state === PENDING) {
-        this.state = REJECTED
-        this.value = val
-        this.fulfilledCallbacks.forEach(fn => fn())
-      }
+  while (current || stack.length > 0) {
+    while (current) {
+      stack.push(current.left)
+      current = current.left
     }
 
-    try {
-      fn(resolve, reject)
-    } catch (r) {
-      reject(r)
-    }
+    current = stack.pop()
+    result.push(current.val)
+    current = current.right
   }
 
-  then(onFulfilled, onRejected) {
-    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : () => {}
-    onRejected =
-      typeof onRejected === 'function'
-        ? onRejected
-        : r => {
-            throw r
-          }
-
-    const promise2 = new MyPromise((resolve, reject) => {})
-    return promise2
-  }
+  return result
 }
