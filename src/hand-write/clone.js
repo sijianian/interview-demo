cf935b4b8a6b80256db92ad1216f2dc309b628ba
moke <moke@xiaoman.cn>
2019-08-26 14:54:29 +0800

    feat: add deepclone

--- src/hand-write/clone.js
+++ src/hand-write/clone.js
@@ -1,4 +1,4 @@
-function shallowClone(obj) {
+const shallowClone = obj => {
   const result = {}
 
   for (let i in obj) {
@@ -7,4 +7,61 @@ function shallowClone(obj) {
   return result
 }
 
-function deepClone() {}
+const deepClone = parent => {
+  const parents = []
+  const children = []
+
+  const getType = obj => {
+    return Object.prototype.toString
+      .call(obj)
+      .replace(/^\[object\s(\w+)\]$/, '$1')
+      .toLowerCase()
+  }
+  const getRegExp = reg => ''
+  const _clone = parent => {
+    if (parent === null) {
+      return null
+    }
+
+    if (typeof parent !== 'object') {
+      return parent
+    }
+
+    let child = null
+    let proto = null
+    let type = getType(parent)
+
+    switch (type) {
+      case 'array':
+        child = []
+        break
+      case 'regexp':
+        child = new RegExp(parent.source, getRegExp(parent))
+        break
+      case 'date':
+        child = new Date(parent.getTime())
+        break
+      default:
+        proto = Object.getPrototypeOf(parent)
+        child = Object.create(proto)
+        break
+    }
+
+    const index = parents.indexOf(parent)
+
+    if (~index) {
+      return children[index]
+    }
+
+    parents.push(parent)
+    children.push(child)
+
+    for (let i in parent) {
+      child[i] = _clone(parent[i])
+    }
+
+    return child
+  }
+
+  return _clone(parent)
+}
---
+++ temp.md
@@ -0,0 +1,8 @@
+script start
+async2 end
+Promise
+script end
+promise1
+promise2
+async1 end
+setTimeout
