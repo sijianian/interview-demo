da529cfb8fc2309a0c12b168c0e5a1fd826b0940
moke <moke@xiaoman.cn>
2019-09-03 10:50:36 +0800

    fix: modify temp.js

--- temp.js
+++ temp.js
@@ -1,19 +1,45 @@
-function inorderTraversal(root) {
-  const stack = []
-  const result = []
-
-  let current = root
-
-  while (current || stack.length > 0) {
-    while (current) {
-      stack.push(current.left)
-      current = current.left
-    }
-
-    current = stack.pop()
-    result.push(current.val)
-    current = current.right
+class EventEmitter {
+  constructor() {
+    this._events = Object.create(null)
   }
 
-  return result
+  on(type, handler) {
+    this._events[type] = this._events[type] || []
+    this._events[type].push(handler)
+  }
+
+  once(type, handler) {
+    let fired = false
+
+    function magic() {
+      this.off(type, magic)
+
+      if (!fired) {
+        handler.apply(this, arguments)
+        fired = true
+      }
+    }
+
+    this.on(type, magic)
+  }
+
+  off(type, handler) {
+    if (this._events[type]) {
+      const index = this._events[type].indexOf(handler)
+
+      if (~index) {
+        this._events[type].splice(index, 1)
+      }
+    }
+  }
+
+  emit(type) {
+    const payload = [...arguments].slice(1)
+
+    if (this._events[type]) {
+      this._events[type].forEach(handler => {
+        handler.apply(this, payload)
+      })
+    }
+  }
 }
