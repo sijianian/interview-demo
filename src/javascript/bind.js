a4d3f989e9d658049efe709cfeba02cf92afeb64
moke <moke@xiaoman.cn>
2019-09-12 09:39:36 +0800

    fix: modify

--- src/javascript/bind.js
+++ src/javascript/bind.js
@@ -5,10 +5,9 @@ export function myBind(context, ...args) {
 
   context = context || window
 
-  // eslint-disable-next-line @typescript-eslint/no-this-alias
   const _this = this
 
-  return function F(args2) {
+  return function F(...args2) {
     if (this instanceof F) {
       return new _this(...args, ...args2)
     }
