ac00eb795553a8db9c41550de557eb3087eeaad7
moke <moke@xiaoman.cn>
2019-09-30 16:24:03 +0800

    :art: add design pattern

---
+++ src/design-mode/singleton-pattern.js
@@ -0,0 +1,22 @@
+// class 实现
+export class MySingle {
+  static getInstance() {
+    if (!MySingle.instance) {
+      MySingle.instance = new MySingle()
+    }
+
+    return MySingle.instance
+  }
+}
+
+// 闭包实现
+MySingle.getInstance = (function() {
+  let instance = null
+
+  return function() {
+    if (!instance) {
+      instance = new MySingle()
+    }
+    return instance
+  }
+})()
