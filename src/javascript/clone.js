915e5d2d5b4b38c659fcc5b8e0d8491b0671f585
moke <444295467@qq.com>
2019-10-08 22:57:51 +0800

    :bug: modify

--- src/javascript/clone.js
+++ src/javascript/clone.js
@@ -7,51 +7,47 @@ export const shallowClone = obj => {
   return result
 }
 
-export const deepClone = parent => {
-  const getType = obj => {
-    return Object.prototype.toString
-      .call(obj)
-      .replace(/^\[object\s(\w+)\]$/, '$1')
-      .toLowerCase()
+export const getType = obj =>
+  Object.prototype
+    .toString(obj)
+    .replace(/^\[object\s(w+)\]$/, '$1')
+    .toLowerCase()
+
+export const deepClone = (parent, hash = new WeakMap()) => {
+  if (parent === null) {
+    return null
   }
 
-  const _clone = (parent, hash = new WeakMap()) => {
-    if (parent === null) {
-      return null
-    }
-
-    if (typeof parent !== 'object') {
-      return parent
-    }
-
-    let child
-    const Constructor = parent.constructor
-
-    switch (getType(parent)) {
-      case 'array':
-        child = []
-        break
-      case 'regexp':
-        child = new Constructor(parent)
-        break
-      case 'date':
-        child = new Date(parent.getTime())
-        break
-      default:
-        if (hash.has(parent)) {
-          return hash.get(parent)
-        }
-        child = new Constructor()
-        hash.set(parent, child)
-        break
-    }
-
-    for (const key in parent) {
-      child[key] = _clone(parent[key], hash)
-    }
-
-    return child
+  if (typeof parent !== 'object') {
+    return parent
   }
 
-  return _clone(parent)
+  let child
+  const type = getType(parent)
+  const ConFn = parent.constructor
+
+  switch (type) {
+    case 'array':
+      child = []
+      break
+    case 'regexp':
+      child = new ConFn(parent)
+      break
+    case 'date':
+      child = new Date(parent.getTime())
+      break
+    default:
+      if (hash.has(parent)) {
+        return hash.get(parent)
+      }
+      child = new ConFn()
+      hash.set(parent, child)
+      break
+  }
+
+  for (const key in parent) {
+    child[key] = deepClone(parent[key], hash)
+  }
+
+  return child
 }
