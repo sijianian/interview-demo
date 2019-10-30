183b668de3f596521170091b6cc84c85a183e958
moke <444295467@qq.com>
2019-10-30 23:27:36 +0800

    :bug: fix: modify curry.js

--- src/javascript/curry.js
+++ src/javascript/curry.js
@@ -1,5 +1,8 @@
-const curry = (fn, arity = fn.length, ...args) =>
-  arity <= args.length ? fn(...args) : curry.bind(null, fn, arity, ...args)
+const curry = (fn, arity = fn.length, ...args) => {
+  return arity <= args.length
+    ? fn(...args)
+    : curry.bind(null, fn, arity, ...args)
+}
 
 console.log(curry(Math.pow)(2)(8))
 console.log(curry(Math.min, 3)(10)(50)(2))
