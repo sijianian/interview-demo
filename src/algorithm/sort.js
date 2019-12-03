a273eaa6363ca0cd310be1a71114a30a0a01e559
moke <moke@xiaoman.cn>
2019-12-03 09:17:33 +0800

    :bug: fix: sort.js

--- src/algorithm/sort.js
+++ src/algorithm/sort.js
@@ -54,7 +54,7 @@ export function selection(array) {
 
   for (let i = 0; i < array.length - 1; i++) {
     let minIndex = i
-    for (let j = i + 1; i < array.length; j++) {
+    for (let j = i + 1; j < array.length; j++) {
       minIndex = array[j] < array[minIndex] ? j : minIndex
     }
     swap(array, i, minIndex)
