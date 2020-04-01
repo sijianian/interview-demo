60e300db1dc5228c06b82c3c46407e102ce054c2
moke <moke@xiaoman.cn>
2020-04-01 11:22:54 +0800

    :zap: feat: add promise-simple

---
+++ src/javascript/promise-simple.js
@@ -0,0 +1,61 @@
+const PENDING = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED = 'rejected'
+
+export class MyPromise {
+  constructor(fn) {
+    this.value = null
+    this.state = PENDING
+
+    this.onFulfilledList = []
+    this.onRejectedList = []
+
+    const resolve = value => {
+      setTimeout(() => {
+        if (this.state === PENDING) {
+          this.value = value
+          this.state = FULFILLED
+          this.onFulfilledList.forEach(fn => fn())
+        }
+      })
+    }
+    const reject = value => {
+      setTimeout(() => {
+        if (this.state === PENDING) {
+          this.value = value
+          this.state = REJECTED
+          this.onRejectedList.forEach(fn => fn())
+        }
+      })
+    }
+
+    try {
+      fn(resolve, reject)
+    } catch (error) {
+      reject(error)
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : v => v
+    onRejected =
+      typeof onRejected === 'function'
+        ? onRejected
+        : r => {
+            throw r
+          }
+
+    if (this.state === PENDING) {
+      this.onFulfilledList.push(onFulfilled)
+      this.onRejectedList.push(onRejected)
+    }
+
+    if (this.state === FULFILLED) {
+      onFulfilled(this.value)
+    }
+
+    if (this.state === REJECTED) {
+      onRejected(this.value)
+    }
+  }
+}
