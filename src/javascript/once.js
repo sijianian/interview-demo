a3ddb05af1534c6d60ea064f7cfcd55943bfce65
moke <444295467@qq.com>
2020-04-02 23:52:02 +0800

    :fire: feat: add src/javascript/once.js

---
+++ src/javascript/once.js
@@ -0,0 +1,11 @@
+export function once(fn) {
+  let called = false
+
+  return function() {
+    if (!called) {
+      called = true
+      // eslint-disable-next-line prefer-rest-params
+      fn.apply(this, arguments)
+    }
+  }
+}
